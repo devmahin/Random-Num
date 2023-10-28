88fe6db61c236ebf3ef7eaaa91461c260d077e92
devmahin <mahinhowlader123@gmail.com>
2023-10-28 08:51:14 -0700

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const ques = document.querySelector(".ques")
 const form = document.querySelector(".form")
 const input = document.querySelector("#input")
-    let stores;
+let scorenum = document.querySelector(".scorenum")
+let getlocal = localStorage.getItem("score")
 
-
+    let totalans;
+    let score = 0;
     function randomNumber (min , max){
         return Math.floor(Math.random() * (max - min + 1) + min);
         console.log(random)
@@ -11,10 +13,47 @@ const input = document.querySelector("#input")
 
 
     function genaredFun (){
-       let ran1=  randomNumber(1, 10)
+       let ran1=  randomNumber(1, 20)
        let ran2 = randomNumber(1, 10)
-       let que = `Question. What is ${ran1} multiply by ${ran2}?`
-       let ans = ran1 * ran2
+       let questionType = randomNumber(1, 4)
+      
+       let firstNum;
+       let lastnum;
+       if(ran1 > ran2 && questionType > 2){
+        firstNum = ran1
+        lastnum = ran2
+        }else{
+        firstNum = ran2
+        lastnum = ran1
+        }
+
+       let que;
+       let ans;
+      
+        switch(questionType){
+            case 1:
+                que = `Question. What is ${firstNum} multiply by ${lastnum}?`
+                ans = firstNum * lastnum
+                break
+            case 2:
+                que = `Question. What is ${firstNum} add ${lastnum}?`
+                ans = firstNum + lastnum
+                break
+            case 3:
+                que = `Question. What is ${firstNum} Divided by ${lastnum}?`
+                ans = firstNum / lastnum
+                break
+            case 4:
+                que = `Question. What is ${firstNum} subtract by ${lastnum}?`
+                ans = firstNum - lastnum
+                break
+        default:
+            que = `Question. What is ${firstNum} multiply by ${lastnum}?`
+            ans = firstNum * lastnum
+        }
+       
+
+
        return {que, ans}
     }
 
@@ -22,9 +61,43 @@ const input = document.querySelector("#input")
     function showQue (){
         let {que, ans} = genaredFun()
         ques.innerText = que
-        stores = ans
+        totalans = ans
+        scorenum.innerText = getlocal
     }
     showQue()
 
 
 
+    function submitFun (event){
+        event.preventDefault()   
+        let data = new FormData(form)
+        let formData = data.get("answer")
+        let usernum = +formData
+        if(totalans === usernum){
+            score++
+            Toastify({
+                text: "Currect Answer",
+                className: "info",
+                gravity: "bottom",
+                position:"center",
+                style: {
+                  background: "linear-gradient(to right, #00b09b, #96c93d)",
+                }
+              }).showToast();
+        }else{
+            score--;
+            Toastify({
+                text: "Not match ",
+                className: "info",
+                gravity: "bottom",
+                position:"center",
+                style: {
+                  background: "linear-gradient(to right, #e33217, #ff001e)",
+                }
+              }).showToast();
+        }
+        showQue()
+         scorenum.innerText = score
+         localStorage.setItem("score", score)
+         event.target.reset()
+        }
